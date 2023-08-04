921d1d0e0399fa2b4cd113c4d74ec462640803cf
sonderbyyonder <30906551+sonderbyyonder@users.noreply.github.com>
2023-08-04 16:31:08 +0800

    implemented gridTemplateLayout

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -25,11 +25,26 @@ const Navbar = (props: Props) => {
       {/* RIGHT SIDE */}
       <FlexBetween gap="2rem">
         {/* why am i getting an error? */}
-        <Box sx={{ "&:hover": { color: palette.primary[200] } }}></Box>
-        <Link to="/" onClick={() => setSelected("dashboard")}>
-          dashboard
-        </Link>
-        <Box></Box>
+        {/* fixed, i needed the import even though they told me i didn't. */}
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link to="/" onClick={() => setSelected("dashboard")} 
+          style = {{
+            color: selected ==="dashboard" ? "inherit" : palette.grey[700],
+            textDecoration: "inherit"
+          }}>
+            dashboard
+          </Link>
+        </Box>
+        {/* predictions/alt segment for the navbar */}
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link to="/" onClick={() => setSelected("predictions")} 
+          style = {{
+            color: selected ==="predictions" ? "inherit" : palette.grey[700],
+            textDecoration: "inherit"
+          }}>
+            predictions
+          </Link>
+        </Box>
       </FlexBetween>
     </FlexBetween>
   );

e21d933ee970688214d3777e14955a9e04e22fe2
sonderbyyonder <30906551+sonderbyyonder@users.noreply.github.com>
2023-08-19 01:29:52 +0800

    made changes for Amplify AWS deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       // ChatGPT resolution for deprecated code?
-      '@': path.resolve(__dirname, "src"),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "src"),
+      find: "./runtimeConfig",
+      replacement: "./runtimeConfig.browser",
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+});
